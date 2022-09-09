f2abc8a5895ed11e32e493adb1abc8582ddab178
Chappo050 <matthewchaplin60@gmail.com>
2022-09-09 21:04:25 +1000

    s

--- app.js
+++ app.js
@@ -73,6 +73,12 @@ if (process.env.NODE_ENV === "production") {
   
   app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
+ });
+
 }
 
 app.use(express.static(path.join(__dirname, "public")));
