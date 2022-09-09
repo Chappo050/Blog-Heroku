e698cf3b5d15e3e2743d31914f7605740c6882f4
Chappo050 <matthewchaplin60@gmail.com>
2022-09-09 20:28:53 +1000

    stuff

--- app.js
+++ app.js
@@ -44,18 +44,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 
-//Production set up
-if (process.env.NODE_ENV === "production") {
-  
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-
- });
-
-}
 
 
 //Middleware
@@ -86,6 +75,20 @@ app.use(compression()); //Compress all routes
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/blog", blogRouter);
+
+//Production set up
+if (process.env.NODE_ENV === "production") {
+  
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
+ });
+
+}
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
